4622dff36f63777c5d175bfb729df1cf6e333f6e
festsnusa <lukatimof@mail.ru>
2023-05-23 21:55:19 +0700

    player progress bar

--- src/components/AppPlayer.vue
+++ src/components/AppPlayer.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 div(class="fixed bottom-0 left-0 bg-white px-4 py-2 w-full")
   // Track Info
-  .text-center
-    span.song-title.font-bold Song Title 
+  .text-center(v-if="current_song.modified_name")
+    span.song-title.font-bold {{ current_song.modified_name }} 
     span by 
-    span.song-artist Artist
+    span.song-artist {{current_song.display_name}}
   div(class="flex flex-nowrap gap-4 items-center")
     // Play/Pause Button
     button(type="button" @click.prevent="toggleAudio")
@@ -14,10 +14,10 @@ div(class="fixed bottom-0 left-0 bg-white px-4 py-2 w-full")
     // Scrub Container
     div(class="w-full h-2 rounded bg-gray-200 relative cursor-pointer")
       // Player Ball
-      span(class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg" style="left: 50%")
+      span(class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg" :style="{left: playerProgress}")
         i.fas.fa-circle
       // Player Progress Bar
-      span(class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400" style="width: 50%")
+      span(class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400" :style="{width: playerProgress}")
     // Duration
     .player-duration {{ duration }}
 </template>
@@ -31,7 +31,13 @@ export default {
     ...mapActions(usePlayerStore, ["toggleAudio"]),
   },
   computed: {
-    ...mapState(usePlayerStore, ["playing", "duration", "seek"]),
+    ...mapState(usePlayerStore, [
+      "playing",
+      "duration",
+      "seek",
+      "playerProgress",
+      "current_song",
+    ]),
   }
 }
 </script>
