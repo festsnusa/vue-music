8512b918def90395fd04cd7a47eebd8d66ba9182
festsnusa <lukatimof@mail.ru>
2023-05-23 19:23:30 +0700

    added pug

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -1,18 +1,12 @@
-<template>
-  <li class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50">
-    <div>
-      <RouterLink :to="{ name: 'song', params: { id: song.docID } }" class="font-bold block text-gray-600">{{
-        song.modified_name }}</RouterLink>
-      <span class="text-gray-500 text-sm">{{ song.display_name }}</span>
-    </div>
+<template lang="pug">
+li(class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50")
+  div
+    RouterLink(:to="{ name: 'song', params: { id: song.docID } }" class="font-bold block text-gray-600") {{ song.modified_name }}
+    span.text-gray-500.text-sm {{ song.display_name }}
 
-    <div class="text-gray-600 text-lg">
-      <span class="comments">
-        <i class="fa fa-comments text-gray-600"></i>
-        {{ song.comment_count }}
-      </span>
-    </div>
-  </li>
+  .text-gray-600.text-lg
+    span.comments
+      i.fa.fa-comments.text-gray-600 {{ song.comment_count }}
 </template>
 
 <script>
