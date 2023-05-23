d11ce2c84906047242d7f0acce80276f773317d9
festsnusa <lukatimof@mail.ru>
2023-05-23 22:22:09 +0700

    added main section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,23 @@
 <template lang="pug">
-section.mb-8.py-20.text-white.text-center.relative
-  .absolute.inset-0.w-full.h-full.bg-contain.introduction-bg(style="background-image: url(assets/img/header.png)")
-  .container.mx-auto
-    .text-white.main-header-content
-      h1.font-bold.text-5xl.mb-5 Listen to Great Music!
-      p(class="w-full md:w-8/12 mx-auto") Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-        | et dolor mollis, congue augue non, venenatis elit. Nunc justo eros,
-        | suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna
-        | pulvinar, fringilla lorem eget, ullamcorper urna.
-  img(class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full" src="/assets/img/introduction-music.png" alt="introduction")
+main
+  section.mb-8.py-20.text-white.text-center.relative
+    .absolute.inset-0.w-full.h-full.bg-contain.introduction-bg(style="background-image: url(assets/img/header.png)")
+    .container.mx-auto
+      .text-white.main-header-content
+        h1.font-bold.text-5xl.mb-5 Listen to Great Music!
+        p(class="w-full md:w-8/12 mx-auto") Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          | et dolor mollis, congue augue non, venenatis elit. Nunc justo eros,
+          | suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna
+          | pulvinar, fringilla lorem eget, ullamcorper urna.
+    img(class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full" src="/assets/img/introduction-music.png" alt="introduction")
 
-section.container.mx-auto
-  .bg-white.rounded.border.border-gray-200.relative.flex.flex-col
-    .px-6.pt-6.pb-5.font-bold.border-b.border-gray-200
-      span.card-title Songs
-      i.fa.fa-headphones-alt.float-right.text-green-400.text-xl
-    ol#playlist
-      SongItem(v-for="song in songs" :key="song.docID" :song="song")
+  section.container.mx-auto
+    .bg-white.rounded.border.border-gray-200.relative.flex.flex-col
+      .px-6.pt-6.pb-5.font-bold.border-b.border-gray-200
+        span.card-title Songs
+        i.fa.fa-headphones-alt.float-right.text-green-400.text-xl
+      ol#playlist
+        SongItem(v-for="song in songs" :key="song.docID" :song="song")
 </template>
 
 <script>
--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -1,46 +1,47 @@
 <template lang="pug">
-// Music Header
-section(class="w-full mb-8 py-14 text-center text-white relative")
-  div(class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
-    style="background-image: url(/assets/img/song-header.png)")
-  div(class="container mx-auto flex items-center")
-    // Play/Pause Button
-    button(type="button" class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
-    @click.prevent="newSong(song)")
-      i.fas.fa-play
-    div(class="z-50 text-left ml-8")
-      //Song Info
-      .text-3xl.font-bold {{ song.modified_name }}
-      div {{ song.genre }}
-// Form
-section.container.mx-auto.mt-6#comments
-  div(class="bg-white rounded border border-gray-200 relative flex flex-col")
-    div(class="px-6 pt-6 pb-5 font-bold border-b border-gray-200")
-      // comment count
-      span.card-title Comments: ({{ song.comment_count }})
-      i(class="fa fa-comments float-right text-green-400 text-2xl")
-    .p-6
-    div(class="text-white text-center font-bold p-4 mb-4" v-if="comment_show_alert" :class="comment_alert_variant") {{ comment_alert_message }}
-    VeeForm(:validation-schema="schema" @submit="addComment" v-if="userLoggedIn")
-      VeeField(as="textarea" name="comment"
-        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
-        placeholder="Your comment here...")
-      ErrorMessage.text-red-600(name="comment")
-      button(type="submit" class="py-1.5 px-3 rounded text-white bg-green-600 block"
-        :disabled="comment_in_submission") Submit
-    // Sort Comments
-    select(v-model="sort" class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded")
-      option(value="1") Latest
-      option(value="2") Oldest
+main
+  // Music Header
+  section(class="w-full mb-8 py-14 text-center text-white relative")
+    div(class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
+      style="background-image: url(/assets/img/song-header.png)")
+    div(class="container mx-auto flex items-center")
+      // Play/Pause Button
+      button(type="button" class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
+      @click.prevent="newSong(song)")
+        i.fas.fa-play
+      div(class="z-50 text-left ml-8")
+        //Song Info
+        .text-3xl.font-bold {{ song.modified_name }}
+        div {{ song.genre }}
+  // Form
+  section.container.mx-auto.mt-6#comments
+    div(class="bg-white rounded border border-gray-200 relative flex flex-col")
+      div(class="px-6 pt-6 pb-5 font-bold border-b border-gray-200")
+        // comment count
+        span.card-title Comments: ({{ song.comment_count }})
+        i(class="fa fa-comments float-right text-green-400 text-2xl")
+      .p-6
+      div(class="text-white text-center font-bold p-4 mb-4" v-if="comment_show_alert" :class="comment_alert_variant") {{ comment_alert_message }}
+      VeeForm(:validation-schema="schema" @submit="addComment" v-if="userLoggedIn")
+        VeeField(as="textarea" name="comment"
+          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
+          placeholder="Your comment here...")
+        ErrorMessage.text-red-600(name="comment")
+        button(type="submit" class="py-1.5 px-3 rounded text-white bg-green-600 block"
+          :disabled="comment_in_submission") Submit
+      // Sort Comments
+      select(v-model="sort" class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded")
+        option(value="1") Latest
+        option(value="2") Oldest
 
-// Comments
-ul.container.mx-auto
-  li(class="p-6 bg-gray-50 border border-gray-200" v-for="comment in sortedComments" :key="comment.docID")
-    // Comment Author
-    .mb-5
-      .font-bold {{ comment.name }}
-      time {{ comment.datePosted }}
-      p {{ comment.content }}
+  // Comments
+  ul.container.mx-auto
+    li(class="p-6 bg-gray-50 border border-gray-200" v-for="comment in sortedComments" :key="comment.docID")
+      // Comment Author
+      .mb-5
+        .font-bold {{ comment.name }}
+        time {{ comment.datePosted }}
+        p {{ comment.content }}
 </template>
 
 <script>
