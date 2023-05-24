e300d31f4b24569e903fa925efcf2fa633b40c70
festsnusa <lukatimof@mail.ru>
2023-05-24 12:56:35 +0700

    added fr locale

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from "vue-i18n"
 import en from "@/locales/en.json"
+import fr from "@/locales/fr.json"
 
 export default createI18n({
   locale: "",
   fallbackLocale: "en",
   messages: {
     en,
+    fr,
   }
 })
