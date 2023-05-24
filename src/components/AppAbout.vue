4ad10351e025405e44e9e86d4eae5b81ec715016
festsnusa <lukatimof@mail.ru>
2023-05-24 18:17:13 +0700

    added AppAbout component

---
+++ src/components/AppAbout.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.about
+  h1 This is an about page
+  AppButton
+</template>
+
+<script>
+export default {
+  name: "AppAbout",
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,16 @@
 <template lang="pug">
-.about
-  h1 This is an about page
+AppAbout
 </template>
+
+<script>
+import AppAbout from '@/components/AppAbout.vue'
+export default {
+  name: "AboutView",
+  components: {
+    AppAbout,
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
