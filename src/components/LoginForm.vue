6c7d90bb1fa34228d3a5d34c0d9dc2084ddc4e94
festsnusa <lukatimof@mail.ru>
2023-05-24 15:39:56 +0700

    refactoring

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,29 +1,22 @@
-<template>
-  <div class="text-white text-center font-bold p-4 mb-4" v-if="login_show_alert" :class="login_alert_variant">
-    {{ login_alert_msg }}
-  </div>
-  <VeeForm :validation-schema="loginSchema" @submit="login">
-    <!-- Email -->
-    <div class="mb-3">
-      <label class="inline-block mb-2">Email</label>
-      <VeeField type="email" name="email"
-        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-        placeholder="Enter Email" />
-      <ErrorMessage class="text-red-600" name="email" />
-    </div>
-    <!-- Password -->
-    <div class="mb-3">
-      <label class="inline-block mb-2">Password</label>
-      <VeeField type="password" name="password"
-        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-        placeholder="Password" />
-      <ErrorMessage class="text-red-600" name="password" />
-    </div>
-    <button type="submit" :disabled="login_in_submission"
-      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700">
-      Submit
-    </button>
-  </VeeForm>
+<template lang="pug">
+div(class="text-white text-center font-bold p-4 mb-4" v-if="login_show_alert" :class="login_alert_variant") {{ login_alert_msg }}
+VeeForm(:validation-schema="loginSchema" @submit="login")
+  // Email
+  .mb-3
+    label.inline-block.mb-2 Email
+    VeeField(type="email" name="email"
+      class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+      placeholder="Enter Email")
+    ErrorMessage.text-red-600(name="email")
+  // Password
+  .mb-3
+    label.inline-block.mb-2 Password
+    VeeField(type="password" name="password"
+      class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+      placeholder="Password")
+    ErrorMessage.text-red-600(name="password")
+  button(type="submit" :disabled="login_in_submission"
+    class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700") Submit
 </template>
 
 <script>
